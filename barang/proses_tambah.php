b61ee9359f0d25f4fe1aa4d81aacf2eaab3db8ed
shakirafitri2410 <shakirafitri45marhas@gmail.com>
2025-02-24 15:09:03 +0700

    pesan commit/catatan

---
+++ barang/proses_tambah.php
@@ -0,0 +1,25 @@
+<?php
+
+$id_barang = $_POST['id_barang'];
+$nama_barang = $_POST['nama_barang'];
+$id_jenis = $_POST['id_jenis'];
+$harga = $_POST['harga'];
+$stok = $_POST['stok'];
+
+include '../../config/koneksi.php';
+
+$query = mysqli_query($conn, "INSERT INTO BARANG VALUES(
+     '$id_barang',
+     '$nama_barang',
+     '$id_jenis',
+     '$harga',
+     '$stok' 
+     )");
+
+     if($query){
+        echo "<script>alert('Data berhasil di simpan')<script>";
+        echo "<script>window.location.href='index.php'<script>";
+     }else{
+        echo "<script>alert('Data gagal di simpan')<script>";
+        echo "<script>window.location.href='tambah.php'<script>";
+     }
